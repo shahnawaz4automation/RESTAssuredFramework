44821c9e11d11b19164d47063be7822b7ec411e0
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-08-16 15:23:02 +0530

    CookiesDemo added

--- src/test/java/day3/CookiesDemo.java
+++ src/test/java/day3/CookiesDemo.java
@@ -1,5 +1,41 @@
 package day3;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 public class CookiesDemo {
 
+	//@Test(priority=1)
+	void testCookies() {
+		given()
+		
+		.when()
+		     .get("https://www.google.com/")
+		.then()
+		     .cookie("AEC")
+		     .log().all();
+	}
+	
+	@Test(priority=2)
+	void getCookiesInfo() {
+		Response res = given()
+				
+		.when()
+		     .get("https://www.google.com/");
+		
+		/*String cookie_value = res.getCookie("AEC");
+		System.out.println("Value for cookie is: " + cookie_value);*/
+		
+		Map<String, String> cookies_values =  res.getCookies();
+		
+		for(String k: cookies_values.keySet()) {
+			String cookie_value = res.getCookie(k);
+			System.out.println(k+":  "+cookie_value);
+		}
+	}
 }
